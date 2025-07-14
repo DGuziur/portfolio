73056d4bb0203f0af658fc630957047cfbc18d82
DGuziur <dondxd@o2.pl>
2025-07-15 01:32:29 +0200

    feat: social links

--- src/app/_components/main-nav.tsx
+++ src/app/_components/main-nav.tsx
@@ -1,13 +1,33 @@
 import Link from "next/link";
-
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 export default function MainNav() {
   return (
-    <nav className="sticky top-0 p-3 flex justify-around">
-      <div>
-        <Link href="/">Home</Link>
-        <Link href="/">About</Link>
-        <Link href="cv">Projects</Link>
-        <Link href="cv">CV</Link>
+    <nav className="sticky top-0 z-10 p-3 flex justify-around">
+      <div className="flex text-2xl gap-6 justify-end w-full">
+        <Link className="hover:opacity-80" href="/">
+          Home
+        </Link>
+        <Link className="hover:opacity-80" href="/">
+          About
+        </Link>
+        <Link className="hover:opacity-80" href="/">
+          Projects
+        </Link>
+        <Link className="hover:opacity-80" href="cv">
+          CV
+        </Link>
+      </div>
+      <div className="flex justify-end gap-3 w-full">
+        <Link className="hover:opacity-80" href="https://github.com/DGuziur">
+          <GitHubIcon></GitHubIcon>
+        </Link>
+        <Link
+          className="hover:opacity-80"
+          href="https://www.linkedin.com/in/dawid-guziur-b33103324/"
+        >
+          <LinkedInIcon></LinkedInIcon>
+        </Link>
       </div>
     </nav>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import MainNav from "./_components/main-nav";
 
 const geistSans = Geist({
@@ -28,8 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <MainNav />
-        {children}
+        <AppRouterCacheProvider>
+          <MainNav />
+          {children}
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
